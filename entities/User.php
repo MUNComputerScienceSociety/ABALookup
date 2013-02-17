e48841e25e0b8db183267188b40ac1efb226fc32
Adam Murphy <adam.murphy@mun.ca>
2013-02-17 17:38:02 -0330

    Change I do not actually want to commit.
    This will probably be changed.

--- entities/User.php
+++ entities/User.php
@@ -22,6 +22,12 @@ class User
 	protected $code_of_conduct;
 	protected $ABA_course;
 	protected $verified;
+	protected $location = null;
+	
+	public function __construct()
+	{
+		$this->location = new ArrayCollection();
+	}
 	
 	public function getId()
 	{
