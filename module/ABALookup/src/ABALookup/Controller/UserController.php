0440fe92b1b4acc296b4b661ecdefe28b21be6e2
Gerry Hall <gerry.j.hall@gmail.com>
2013-02-18 12:28:00 -0330

    changes to register

--- module/ABALookup/src/ABALookup/Controller/UserController.php
+++ module/ABALookup/src/ABALookup/Controller/UserController.php
@@ -16,9 +16,48 @@ use
 ;
 
 class UserController extends ABALookupController {
-	public function registerAction() {
-		return new ViewModel();
+
+    public function registerAction() {
+
+        if (isset ($_POST['submit'])) {
+            $usertype = $_POST['usertype'];
+            $username = $_POST['username'];
+            $emailaddress = $_POST['emailaddress'];
+            $password = $_POST['password'];
+            $confirmpassword = $_POST['confirmpassword'];
+
+            if (empty($usertype) || empty($username) || empty($emailaddress) ||
+                empty($password) || empty($confirmpassword)) {
+                return new ViewModel(array(
+                    "error" => "All fields are required",
+                    "usertype" => $usertype,
+                    "username" => $username,
+                    "emailaddress" => $emailaddress,
+                ));
+            }
+
+            if ($confirmpassword != $password) {
+                return new ViewModel(array(
+                    "error" => "Your passwords do not match",
+                    "usertype" => $usertype,
+                    "username" => $username,
+                    "emailaddress" => $emailaddress,
+                ));
+            }
+
+            if (strlen($password) < 6) {
+                return new ViewModel(array(
+                    "error" => "Your password must be at least 6 characters in length",
+                    "usertype" => $usertype,
+                    "username" => $username,
+                    "emailaddress" => $emailaddress,
+                ));
+            }
+        }
+
+        return new ViewModel();
 	}
+
 	public function loginAction() {
 		return new ViewModel();
 	}
