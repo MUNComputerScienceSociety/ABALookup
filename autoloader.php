64d8422a831fbd69d9b7fec6903cf590b2d7a333
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-06-09 13:06:01 -0400

    Removed ZF comment from autoloader.

--- autoloader.php
+++ autoloader.php
@@ -1,13 +1,5 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-/**
  * This autoloading setup is really more complicated than it needs to be for most
  * applications. The added complexity is simply to reduce the time it takes for
  * new developers to be productive with a fresh skeleton. It allows autoloading
