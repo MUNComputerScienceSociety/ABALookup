40fb5603a2e5ad450312cb1f77700655e3aa3c2e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-06-20 13:05:12 -0400

    Added more tests for Location entity.

--- module/AbaLookup/src/AbaLookup/Entity/Location.php
+++ module/AbaLookup/src/AbaLookup/Entity/Location.php
@@ -7,7 +7,9 @@ use
 	Doctrine\ORM\Mapping\Entity as DoctrineEntity,
 	Doctrine\ORM\Mapping\GeneratedValue,
 	Doctrine\ORM\Mapping\Id,
-	Doctrine\ORM\Mapping\Table
+	Doctrine\ORM\Mapping\Table,
+	InvalidArgumentException,
+	Zend\Validator\StringLength
 ;
 
 /**
@@ -36,31 +38,66 @@ class Location
 	protected $enabled;
 
 	/**
+	 * String length validator
+	 */
+	protected static $strlen;
+
+	/**
+	 * Initialise static fields
+	 *
+	 * Called immediately after class definition.
+	 */
+	public static function init()
+	{
+		self::$strlen = new StringLength(['min' => 1]);
+	}
+
+	/**
 	 * Constructor
 	 */
 	public function __construct($name)
 	{
+		if (!is_string($name) || !self::$strlen->isValid($name)) {
+			throw new InvalidArgumentException();
+		}
 		$this->name = $name;
 		$this->enabled = TRUE;
 	}
 
 	/**
 	 * Set the name of the location
+	 *
+	 * @param string $name The name of the location.
+	 * @return $this
 	 */
 	public function setName($name)
 	{
+		if (!self::$strlen->isValid($name)) {
+			throw new InvalidArgumentException();
+		}
 		$this->name = $name;
 		return $this;
 	}
 
+	/**
+	 * Set the enabled value of the location
+	 *
+	 * @param bool $enabled Whether this location is enabled.
+	 * @return $this
+	 */
 	public function setEnabled($enabled)
 	{
+		if (!is_bool($enabled)) {
+			throw new InvalidArgumentException();
+		}
 		$this->enabled = $enabled;
 		return $this;
 	}
 
 	/**
 	 * Enable the location
+	 *
+	 * @return $this
 	 */
 	public function enable()
 	{
@@ -70,6 +107,8 @@ class Location
 
 	/**
 	 * Disable the location
+	 *
+	 * @return $this
 	 */
 	public function disable()
 	{
@@ -77,18 +116,29 @@ class Location
 		return $this;
 	}
 
+	/**
+	 * @return int $id
+	 */
 	public function getId()
 	{
 		return $this->id;
 	}
 
+	/**
+	 * @return string $name
+	 */
 	public function getName()
 	{
 		return $this->name;
 	}
 
+	/**
+	 * @return bool $enabled
+	 */
 	public function getEnabled()
 	{
 		return $this->enabled;
 	}
 }
+
+Location::init();
